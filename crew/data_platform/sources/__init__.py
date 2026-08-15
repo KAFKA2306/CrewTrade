b089eb0060089ed899f6ce36a4a729087df6a49b
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-15 19:09:49 +0900

    Register JPX ETF source

--- crew/data_platform/sources/__init__.py
+++ crew/data_platform/sources/__init__.py
@@ -1,4 +1,5 @@
 from crew.data_platform.sources.fred import FredSource
+from crew.data_platform.sources.jpx import JpxEtfMasterSource
 from crew.data_platform.sources.manual import GovernedManualSource
 from crew.data_platform.sources.sec import SecSource
 from crew.data_platform.sources.treasury import TreasuryYieldCurveSource
@@ -6,6 +7,7 @@ from crew.data_platform.sources.treasury import TreasuryYieldCurveSource
 __all__ = [
     "FredSource",
     "GovernedManualSource",
+    "JpxEtfMasterSource",
     "SecSource",
     "TreasuryYieldCurveSource",
 ]
